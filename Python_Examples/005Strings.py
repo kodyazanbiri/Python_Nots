9fb4275437a90724fe1238ca042639b34e699fa1
Suna Gülmez <109751575+sunaglmez@users.noreply.github.com>
2024-07-04 15:26:32 +0300

    Create 005Strings.py

---
+++ Python_Examples/005Strings.py
@@ -0,0 +1,16 @@
+first_name = 'Ali'
+last_name = 'Veli'
+person_age = 28
+
+message = 'Hello, my name is ' + first_name + ' ' + last_name + ' and \nI am ' + str(person_age) + ' years old'
+message_length = len(message)
+
+# print(message)
+# print(message[0])
+# print(message[3])
+# print(message[message_length-1])
+# print(message[-1])
+# print(message[3:7])
+# print(message[3:])
+# print(message[:16])
+print(message[2:40:3])
