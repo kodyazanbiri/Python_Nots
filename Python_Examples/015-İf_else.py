d8d8e6fc4c4d7a44ac04acc2570c36daf9a830c4
Suna Gülmez <109751575+sunaglmez@users.noreply.github.com>
2024-07-12 14:44:34 +0300

    Create 015-İf_else.py

---
+++ Python_Examples/015-İf_else.py
@@ -0,0 +1,12 @@
+user_id = 'suna123'
+user_pass = 'securepassword'
+
+if (user_id == 'suna123'):
+
+    if (user_pass == 'securepassword'):
+        print('Giriş başarılı, hoş geldiniz!')
+    else:
+        print('Yanlış şifre, lütfen tekrar deneyin.')
+        
+else:
+    print('Kullanıcı adı bulunamadı, kayıt olun.')
